9517063b0a6e3d1a822c815a7854a30af2084139
ZHENG Zhong <heavyzheng@gmail.com>
2012-11-13 21:03:02 +0100

    Setup Xcode project layout.

---
+++ ios/main.m
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "ZZMafiaAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZZMafiaAppDelegate class]));
+    }
+}
