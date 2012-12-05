9ca168efdca2b1c3a6f8c6a6e2b045446b06b056
ZHENG Zhong <heavyzheng@gmail.com>
2012-12-05 11:54:09 +0100

    Fixed eol in source files.

--- ios/Sources/Views/Guide/MafiaHTMLPageController.h
+++ ios/Sources/Views/Guide/MafiaHTMLPageController.h
@@ -1,1 +1,19 @@
-////  Created by ZHENG Zhong on 2012-12-03.//  Copyright (c) 2012 ZHENG Zhong. All rights reserved.//#import <UIKit/UIKit.h>@class MafiaHTMLPage;@interface MafiaHTMLPageController : UIViewController@property (retain, nonatomic) IBOutlet UIWebView *webView;@property (readonly, retain, nonatomic) MafiaHTMLPage *htmlPage;+ (id)controllerWithHTMLPage:(MafiaHTMLPage *)htmlPage;- (id)initWithHTMLPage:(MafiaHTMLPage *)htmlPage;@end // MafiaHTMLPageController
+//
+//  Created by ZHENG Zhong on 2012-12-03.
+//  Copyright (c) 2012 ZHENG Zhong. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+@class MafiaHTMLPage;
+
+
+@interface MafiaHTMLPageController : UIViewController
+
+@property (retain, nonatomic) IBOutlet UIWebView *webView;
+@property (readonly, retain, nonatomic) MafiaHTMLPage *htmlPage;
+
++ (id)controllerWithHTMLPage:(MafiaHTMLPage *)htmlPage;
+
+- (id)initWithHTMLPage:(MafiaHTMLPage *)htmlPage;
+
+@end // MafiaHTMLPageController
+
--- ios/Sources/Views/Guide/MafiaHTMLPageController.m
+++ ios/Sources/Views/Guide/MafiaHTMLPageController.m
@@ -1,1 +1,56 @@
-////  Created by ZHENG Zhong on 2012-12-03.//  Copyright (c) 2012 ZHENG Zhong. All rights reserved.//#import "MafiaHTMLPageController.h"#import "MafiaHTMLPage.h"@implementation MafiaHTMLPageController@synthesize webView = _webView;@synthesize htmlPage = _htmlPage;+ (id)controllerWithHTMLPage:(MafiaHTMLPage *)htmlPage{    return [[[self alloc] initWithHTMLPage:htmlPage] autorelease];}- (void)dealloc{    [_webView release];    [_htmlPage release];    [super dealloc];}- (id)initWithHTMLPage:(MafiaHTMLPage *)htmlPage{    if (self = [super initWithNibName:@"MafiaHTMLPageController" bundle:nil])    {        _htmlPage = [htmlPage retain];        self.title = htmlPage.title;    }    return self;}- (void)viewDidLoad{    [super viewDidLoad];    NSString *resourcePath = [self.htmlPage resourcePath];    NSData *htmlData = [NSData dataWithContentsOfFile:resourcePath];    NSURL *baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]];    if (htmlData)    {        [self.webView loadData:htmlData MIMEType:@"text/html" textEncodingName:@"utf-8" baseURL:baseURL];    }    self.webView.scalesPageToFit = NO;}@end // MafiaHTMLPageController
+//
+//  Created by ZHENG Zhong on 2012-12-03.
+//  Copyright (c) 2012 ZHENG Zhong. All rights reserved.
+//
+#import "MafiaHTMLPageController.h"
+#import "MafiaHTMLPage.h"
+
+
+@implementation MafiaHTMLPageController
+
+
+@synthesize webView = _webView;
+@synthesize htmlPage = _htmlPage;
+
+
++ (id)controllerWithHTMLPage:(MafiaHTMLPage *)htmlPage
+{
+    return [[[self alloc] initWithHTMLPage:htmlPage] autorelease];
+}
+
+
+- (void)dealloc
+{
+    [_webView release];
+    [_htmlPage release];
+    [super dealloc];
+}
+
+
+- (id)initWithHTMLPage:(MafiaHTMLPage *)htmlPage
+{
+    if (self = [super initWithNibName:@"MafiaHTMLPageController" bundle:nil])
+    {
+        _htmlPage = [htmlPage retain];
+        self.title = htmlPage.title;
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSString *resourcePath = [self.htmlPage resourcePath];
+    NSData *htmlData = [NSData dataWithContentsOfFile:resourcePath];
+    NSURL *baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]];
+    if (htmlData)
+    {
+        [self.webView loadData:htmlData MIMEType:@"text/html" textEncodingName:@"utf-8" baseURL:baseURL];
+    }
+    self.webView.scalesPageToFit = NO;
+}
+
+
+@end // MafiaHTMLPageController
+
